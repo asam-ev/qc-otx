db72257636eac1beb4fbf7787ce66b0782947e26
romanodanilo <62891297+romanodanilo@users.noreply.github.com>
2024-07-26 09:46:52 +0200

    Add first data type check (#12)
    Signed-off-by: romanodanilo <danilo@ivex.ai>

---
+++ qc_otx/checks/data_type_checker/__init__.py
@@ -0,0 +1,3 @@
+from . import data_type_constants as data_type_constants
+from . import data_type_checker as data_type_checker
+from . import accessing_structure_elements as accessing_structure_elements
