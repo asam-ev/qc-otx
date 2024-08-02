4bffd505dd663bd21cd257455e5db78840f0d2d6
romanodanilo <62891297+romanodanilo@users.noreply.github.com>
2024-08-02 17:15:07 +0200

    Add mandatory fields checks (#17)
    Signed-off-by: romanodanilo <danilo@ivex.ai>
    Signed-off-by: romanodanilo <62891297+romanodanilo@users.noreply.github.com>

--- qc_otx/checks/state_machine_checker/__init__.py
+++ qc_otx/checks/state_machine_checker/__init__.py
@@ -2,3 +2,9 @@ from . import state_machine_constants as state_machine_constants
 from . import state_machine_checker as state_machine_checker
 from . import no_procedure_realization as no_procedure_realization
 from . import mandatory_target_state as mandatory_target_state
+from . import no_target_state_for_completed_state as no_target_state_for_completed_state
+from . import mandatory_transition as mandatory_transition
+from . import mandatory_trigger as mandatory_trigger
+from . import (
+    distinguished_initial_and_completed_state as distinguished_initial_and_completed_state,
+)
